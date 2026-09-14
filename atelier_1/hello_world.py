94a664cadc349fe2c18699a2ec814e92ca1aa46c
ANA CAROLINA Rossini Monteiro <acrossini@etu.uqac.ca>
2026-09-14 18:46:29 -0400

    adding hello_world.py

---
+++ atelier_1/hello_world.py
@@ -0,0 +1,10 @@
+import sys
+
+this_is_a_string = "je suis un string"
+print(this_is_a_string)
+
+def my_good_function():
+    print("This is a good function!")
+
+my_good_function()
+print("Hello World!")
