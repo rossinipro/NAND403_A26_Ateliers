73ba00c9e963c05929031b3e28fbedf4af67b604
Ana Carolina Rossini <rossini.pro@gmail.com>
2026-09-21 16:40:31 -0400

    Adding Atelier 1

---
+++ atelier_1/Atelier 1.py
@@ -0,0 +1,6 @@
+import sys
+
+my_name = input("What is your name? ")
+my_country = input("What country are you from? ")
+
+print(f"Hello {my_name} from {my_country}!")
--- atelier_1/hello_world.py
+++ atelier_1/hello_world.py
@@ -1,4 +1,21 @@
 import sys
+import json
+
+def main():
+    if len(sys.argv) < 2:
+       print("Usage: python main.py <json_file>")
+       return
+
+    json_file = sys.argv[1]
+
+    with open(json_file, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
 
 this_is_a_string = "je suis un string"
 print(this_is_a_string)
